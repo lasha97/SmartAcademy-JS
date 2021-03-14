5b3b9ebd5d69dea40ae04c023d29193764de0620
lasha97 <lasha97lasha@gmail.com>
2021-03-14 22:45:08 +0400

    add first homework js

---
+++ main.js
@@ -0,0 +1,23 @@
+//First task
+console.log("First Task")
+var x = 3
+var y = -4 
+var sum = x + y
+if(sum%2===0){
+  console.log('Even')
+}
+else{
+  console.log('Odd')
+}
+
+//Second
+console.log("Second task")
+var b = 5
+var h = 2
+
+b=Math.abs(b)
+h=Math.abs(h)
+
+var area=(b*h)/2 
+
+console.log(area)
